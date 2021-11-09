eb15f18712207fa926d3794b7f6061f7697de2ae
ryan woods <federer5050@gmail.com>
2021-11-09 17:30:24 +0000

    adding more images

--- script.js
+++ script.js
@@ -109,4 +109,11 @@ document.addEventListener("DOMContentLoaded", function(event) {
         }
 
 
+
+        // TESTING (call a function on click to set images)
+            setTimeout(function(){
+                document.querySelector('.block__turkish').style.backgroundImage = "url('https://ryan50500.github.io/takeaway/images/indian.jpg')";
+            }, 2000);
+         // TESTING
+
   });
